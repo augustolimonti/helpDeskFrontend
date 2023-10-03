e2cf7efe6e8a26dfd69c78d0fb0ce4d325ebd7ed
Augusto Limonti <agulimonti09@gmail.com>
2023-10-03 18:37:18 +0200

    update

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -14,7 +14,7 @@ const Admin = () => {
   // This useEffect contains a get request to the API to retreive all ticket data to render in admin portal.
   useEffect(() => {
     // GET request
-    axios.get('http://3.17.164.174/')
+    axios.get('http://18.221.23.167:8000/')
       .then(response => {
         console.log(response.data)
         setTicketData(response.data);
